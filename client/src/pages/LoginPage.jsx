83a6af70500d6f5f5d953bfab0624999c539d90e
Perovsky56 <67289269+Perovsky56@users.noreply.github.com>
2023-04-24 10:52:56 +0200

    Login/Register + DB Connection + Basic Register handling

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
-    return "login page should go here";
+    return (
+        <div className="mt-5 grow flex items-center justify-around">
+            <div className="mb-64">
+                <h1 className="mb-4 text-center text-4xl">Logowanie</h1>
+                <form className="mx-auto max-w-md">
+                    <input type="email" placeholder="twój@email.com" />
+                    <input type="password" placeholder="hasło" />
+                    <button className="primary">Zaloguj się</button>
+                    <div className="text-center py-2 text-gray-500">
+                        Nie masz konta? <Link className="underline text-black" to={'/register'}>Zarejestruj się</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
+
+export default function RegisterPage() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function registerUser(ev){
+        ev.preventDefault();
+        try {
+            await axios.post('/register', {
+                name,
+                email,
+                password,
+            });
+            alert('Rejestracja udana. Możesz przejść do logowania.');
+        } catch (e) {
+            alert('Rejestracja nieudana. Spróbuj ponownie później.');
+        }
+    }
+
+    return (
+        <div className="mt-5 grow flex items-center justify-around">
+            <div className="mb-64">
+                <h1 className="mb-4 text-center text-4xl">Rejestracja</h1>
+                <form className="mx-auto max-w-md" onSubmit={registerUser}>
+                    <input type="text"
+                        placeholder="imię nazwisko"
+                        value={name}
+                        onChange={ev => setName(ev.target.value)} />
+                    <input type="email"
+                        placeholder="twój@email.com"
+                        value={email}
+                        onChange={ev => setEmail(ev.target.value)} />
+                    <input type="password"
+                        placeholder="hasło"
+                        value={password}
+                        onChange={ev => setPassword(ev.target.value)} />
+                    <button className="primary">Zarejestruj</button>
+                    <div className="text-center py-2 text-gray-500">
+                        Masz już konto? <Link className="underline text-black" to={'/login'}>Zaloguj się</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
