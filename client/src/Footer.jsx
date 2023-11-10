195ec859aa86cc6531936added2458e1a59dcb41
Perovsky56 <pawcio569721@gmail.com>
2023-11-10 12:01:07 +0100

    Updated Footer

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return (
         <div className="mt-12 bg-black pb-16">
             <div className="p-4">
-                <h3 className="text-gray-400 px-8">Wyświetlana cena jest wartością bazową, która może ulec zmianie w trakcie rezerwacji pojazdu.</h3>
+                <h3 className="text-gray-400 px-8">UWAGA: Wyświetlana cena jest wartością bazową, która może ulec zmianie w trakcie rezerwacji pojazdu.</h3>
             </div>
             <div className="px-8">
                 <hr className="border-orange-500"></hr>
@@ -11,7 +11,7 @@ export default function Footer(){
                 <div className="px-16">
                     <div>
                         <h4 className="text-lg py-4 text-gray-200">| carRental.</h4>
-                        <div className="px-4">
+                        <div className="px-4 text-lg">
                             <p>Oferujemy szeroki wybór pojazdów dostępnych do wynajęcia.</p>
                             <p>Dostarczamy samochody na wybrane adresy przez całą dobę.</p>
                             <p>Rezerwuj już dziś i ciesz się komfortem podróżowania.</p>
@@ -21,7 +21,7 @@ export default function Footer(){
                 <div className="px-16">
                     <div>
                         <h4 className="text-lg py-4 text-gray-200">| KONTAKT</h4>
-                        <div className="px-4">
+                        <div className="px-4 text-lg">
                             <p>Adres: ul. Prószkowska 76, 46-020 Opole</p>
                             <p>Email: p.siemiginowski@student.po.edu.pl</p>
                             <p>Telefon: +48 123 456 789</p>
